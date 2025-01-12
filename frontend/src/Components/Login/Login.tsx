d5cc65b4946599d027116ab1356264673c146dae
HARRIR HABIB ABDELGHANI <100070886+Harrir2Abdelghani@users.noreply.github.com>
2025-01-12 11:49:22 +0100

    Update Login.tsx

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -52,13 +52,8 @@ const Login: React.FC = () => {
       <div className="md:w-1/3 max-w-sm">
         <img src={logo} alt="logo image" />
       </div>
-      <!-- Intentlead -->
-<div style="width: 100%">
-    <script src="https://app.intentlead.com/js/H4o8TAhYdN"></script>
-</div>
-        <!-- Intentlead -->
-<div style="width: 80%">
-    <script src="https://app.intentlead.com/js/H4o8TAhYdN"></script>
+      <div style="width: 100%">
+    <script src="https://app.intentlead.com/js/LU27O84PAQ"></script>
 </div>
       <div className="md:w-1/3 max-w-sm -mt-8">
         <div className="text-center md:text-left">
