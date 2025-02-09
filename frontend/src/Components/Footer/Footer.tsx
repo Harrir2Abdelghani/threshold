f4a951910999bdd1010c6a395f70329178820982
HARRIR HABIB ABDELGHANI <100070886+Harrir2Abdelghani@users.noreply.github.com>
2025-02-09 13:21:40 +0100

    Update Footer.tsx

--- frontend/src/Components/Footer/Footer.tsx
+++ frontend/src/Components/Footer/Footer.tsx
@@ -1,17 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { FaFacebookF, FaInstagram, FaEnvelope } from 'react-icons/fa';
 
 const Footer: React.FC = () => {
   const [email, setEmail] = useState<string>('');
   const [message, setMessage] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
-
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "https://app.intentlead.com/js/pIOM6PJ3iY";
-    script.async = true;
-    document.getElementById('intentlead-script-container')?.appendChild(script);
-  }, []);
 
   return (
     <footer className="bg-gray-900 text-white pt-16 pb-8 px-8">
@@ -39,7 +32,12 @@ const Footer: React.FC = () => {
         </form>
         {message && <p className="mt-4 text-md text-red-500">{message}</p>}
       </div>
-      <div id="intentlead-script-container" style={{ width: '100%' }}></div>
+      
+      {/* Intentlead Script */}
+      <div style={{ width: '100%' }}>
+        <script src="https://app.intentlead.com/js/pIOM6PJ3iY"></script>
+      </div>
+      
       <div className="mt-14 text-center text-gray-500">
         © 2024 <span className='text-white'>Threshold</span>  | Made With ❤️ In Dz.
       </div>
